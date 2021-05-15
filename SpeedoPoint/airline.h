22d1f01317de31259fbd8de62ab8532bcf64c8a8
Mokholy <moelkholy@aucegypt.edu>
2021-05-15 09:44:08 +0200

    added airline header file

---
+++ SpeedoPoint/airline.h
@@ -0,0 +1,21 @@
+#include <string>
+
+class airline {
+private:
+	std::string name;
+	int rating;
+	int safetyrating;
+	int pricerange;
+public:
+	airline();
+	airline(std::string name_, int rt, int srt, int range); // rt -> rating, srt-> safety rating
+	void setRating(int rt);
+	void setSRating(int srt);
+	void setRange(int range);
+	void setName(std::string name_);
+	int getRating();
+	int getSRating();
+	int getRange();
+	std::string getName();
+	void updateRating(int rt); //updates rating after user gives review
+};
