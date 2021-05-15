0821a2067c50160ace4e9f5bb815bc3cd68a14bd
Mokholy <moelkholy@aucegypt.edu>
2021-05-15 11:02:29 +0200

    added flightlisting header file

---
+++ SpeedoPoint/flightlisting.h
@@ -0,0 +1,32 @@
+#pragma once
+#ifndef FLIGHTLISTING_H
+#define FLIGHTLISTING_H
+#include "airline.h"
+#include "stop.h"
+#include "date.h"
+#include <string>
+#include "user.h"
+#include "flightticket.h"
+class flightlisting {
+private:
+	airline airline;
+	stop stops;
+	int stopnum;
+	date dep;
+	date arr;
+	int pricepertraveller;
+	std::string cabin;
+	std::string plane;
+	int carronW;
+	int checkedBaggageW;
+	int additionalBagPrice;
+public:
+	flightlisting();
+	flightlisting(airline air, stop stp, int sptnum, date departure, date arrival, int price, std::string cabin_, std::string planeModel, int carryOn, int CheckedW, int additionalWPrice);
+	flightticket reserve(user acc);
+
+};
+
+
+
+#endif FLIGHTLISTING_H
