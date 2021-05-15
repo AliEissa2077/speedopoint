10eb79100e177d009c494ba54338c7f78489ea08
Mokholy <moelkholy@aucegypt.edu>
2021-05-15 10:32:12 +0200

    added cruise company header file

---
+++ SpeedoPoint/cruisecompany.h
@@ -0,0 +1,20 @@
+#pragma once
+#ifndef CRUISECOMPANY_H
+#define CRUISECOMPANY_H
+#include <string>
+class cruisecompany {
+private:
+	std::string name;
+	int rating;
+	int safetyRating;
+	int priceRange;
+public:
+	cruisecompany();
+	cruisecompany(std::string name_, int rt, int srt, int range);
+	void updateRating(int rt);
+};
+
+
+
+
+#endif CRUISECOMPANY_H
