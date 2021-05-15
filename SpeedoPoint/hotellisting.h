534e2b32405a767bbd13002aed6a0ce914ab5f9c
Mokholy <moelkholy@aucegypt.edu>
2021-05-15 10:54:55 +0200

    added hotel listing header file:

---
+++ SpeedoPoint/hotellisting.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef HOTELLISTING_H
+#define HOTELLISTING_H
+#include "hotel.h"
+#include "country.h"
+#include <string>
+#include "reservation.h"
+#include "user.h"
+
+class hotellisting {
+	hotel hotel;
+	country loc;
+	int cityIndex;
+	int area;
+	bool wifi;
+	bool bfast;
+	int person;
+	bool refundable;
+	bool dinner;
+	bool pets;
+	std::string roomType;
+public:
+	hotellisting();
+	hotellisting(hotel hot, country location, int index, int area_, bool wifi_, bool breakfast, int numPeople, bool refund, bool dinner_, std::string roomtype);
+	reservation reserve(user acc);
+
+};
+
+
+#endif HOTELLISTING_H
