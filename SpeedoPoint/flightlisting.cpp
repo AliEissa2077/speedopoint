12cd62e28da23ac1ab6c3731b5b283d59948f2cc
Yaseenshb <78433853+Yaseenshb@users.noreply.github.com>
2021-05-18 15:32:31 +0200

    hi
    where do i start

---
+++ SpeedoPoint/flightlisting.cpp
@@ -0,0 +1,3 @@
+#include "flightlisting.h"
+
+flightlisting::flightlisting(airline air, stop* stp, int sptnum, date departure, date arrival, int price, std::string cabin_, std::string planeModel, int carryOn, int CheckedW, int additionalWPrice);
---
+++ SpeedoPoint/stop.cpp
@@ -0,0 +1,27 @@
+#include "stop.h"
+
+
+
+
+
+
+
+
+
+
+stop::stop(country location, int index, airline airl, date d)
+{
+
+	loc = location;
+	cityIndex = index;
+	airline = airl;
+	time = d;
+}
+
+
+
+
+
+
+
+#endif STOP_H
