e4f963027e136664e5bee985f7e5eb3ef879e4b9
Yaseenshb <78433853+Yaseenshb@users.noreply.github.com>
2021-05-19 11:51:08 +0200

    cruiseres.cpp

---
+++ SpeedoPoint/cruiseReservation.cpp
@@ -0,0 +1,14 @@
+#include "cruisereservation.h"
+
+
+
+
+cruisereservation::cruisereservation(cruise crs, payment pay, int ad, int child, user user);
+{
+	cruze = crs;
+	paymnt = pay;
+	adults = ad;
+	children = child;
+	acc = user;
+}
+
