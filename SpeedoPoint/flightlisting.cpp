b3e8384956677732b814033bf290b5a064ffaf86
Yaseenshb <78433853+Yaseenshb@users.noreply.github.com>
2021-05-19 11:45:41 +0200

    idk even

--- SpeedoPoint/flightlisting.cpp
+++ SpeedoPoint/flightlisting.cpp
@@ -1,3 +1,10 @@
 #include "flightlisting.h"
 
 flightlisting::flightlisting(airline air, stop* stp, int sptnum, date departure, date arrival, int price, std::string cabin_, std::string planeModel, int carryOn, int CheckedW, int additionalWPrice);
+
+flightlisting::reserve()
+{
+	return 
+}
+
+flightticket reserve(user acc);
