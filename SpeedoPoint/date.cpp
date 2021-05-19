e15487b4e8cc2374fa98c53f5b210b1749790b50
Yaseenshb <78433853+Yaseenshb@users.noreply.github.com>
2021-05-19 20:07:11 +0200

    Date.cpp
    implemented

--- SpeedoPoint/date.cpp
+++ SpeedoPoint/date.cpp
@@ -1,3 +1,40 @@
 #include "date.h"
 
-void main(){}
+int getYear();
+int getMonth();
+int getDay();
+int getHour();
+int getMinute();
+
+
+
+
+int date::getYear()
+{
+	return year;
+}
+
+int date::getMonth()
+{
+	return month;
+}
+
+int date::getDay()
+{
+	return day;
+}
+
+int date::getHour()
+{
+	return hour;
+}
+
+int date::getMinute()
+{
+	return minute;
+}
+
+date::date(int d, int m, int y, int h, int min)
+{
+
+}
