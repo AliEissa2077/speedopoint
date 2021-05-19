6170d4720ea3f47a9fa2fe77db06dde649dc1b9e
sandraeshra <76903747+sandraeshra@users.noreply.github.com>
2021-05-19 20:09:52 +0200

    date and country implentation

--- SpeedoPoint/country.cpp
+++ SpeedoPoint/country.cpp
@@ -1,3 +1,41 @@
 #include "country.h"
 
-void main(){}
+country::country()
+{
+
+}
+
+
+country::country(std::string name_, bool ban)
+{
+	name = name_;
+	banned = ban;
+}
+
+void country:: addCity(std::string city)
+{
+	bool flag = false;
+	for (auto x : cities)
+	{
+		if (x == city)
+		{
+			flag = true;
+		}
+	}
+
+	if (!flag)
+	{
+		cities.push_back(city);
+	}
+}
+
+std::string country::getName()
+{
+	return name;
+}
+
+std::vector<std::string>country::getCities()
+{
+	return cities;
+}
+
--- SpeedoPoint/date.cpp
+++ SpeedoPoint/date.cpp
@@ -1,3 +1,47 @@
 #include "date.h"
 
-void main(){}
+date::date()
+{
+
+}
+
+date::date(int d, int m, int y, int h, int min)
+{
+	day = d;
+	month = m;
+	year = y;
+	hour = h;
+	minute = min;
+}
+
+date::date(int d, int m, int y)
+{
+	day = d;
+	month = m;
+	year = y;
+}
+
+int date::getYear()
+{
+	return year;
+}
+
+int date::getMonth()
+{
+	return month;
+}
+
+int date::getDay()
+{
+	return day;
+}
+
+int date::getHour()
+{
+	return hour;
+}
+
+int date::getMinute()
+{
+	return minute;
+}
