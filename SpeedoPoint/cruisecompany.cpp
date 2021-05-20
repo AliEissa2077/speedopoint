758f70bb9263fc3c86d3ef9194a48986c90925b5
sandraeshra <76903747+sandraeshra@users.noreply.github.com>
2021-05-20 11:18:10 +0200

    cruise company and cruise reservation . cpp

--- SpeedoPoint/cruisecompany.cpp
+++ SpeedoPoint/cruisecompany.cpp
@@ -1,1 +1,19 @@
 #include "cruisecompany.h"
+
+cruisecompany::cruisecompany()
+{
+
+}
+
+cruisecompany::cruisecompany(std::string name_, int rt, int srt, int range)
+{
+	std::string name = name_;
+	rating = rt;
+	safetyRating = srt;
+	priceRange = range;
+}
+
+void cruisecompany::updateRating(int rt)
+{
+	rating = rating + rt;
+}
--- SpeedoPoint/cruisereservation.h
+++ SpeedoPoint/cruisereservation.h
@@ -1,9 +1,9 @@
 #pragma once
 #ifndef CRUISERESERVATION_H
 #define CRUISERESERVATION_H
-//#include "cruise.h"
+#include "cruise.h"
 #include "payment.h"
-//#include "user.h"
+#include "user.h"
 
 class cruise;
 class user;
